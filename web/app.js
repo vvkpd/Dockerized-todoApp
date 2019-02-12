fca1ebf701d4fd1233fae2dd284bc90012ef93d1
Vivek Kumar Pandey <vivekp@thoughtworks.com>
2019-02-12 18:42:00 +0530

    integrated api todo endpoint to web todo  endpoint

--- web/app.js
+++ web/app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const request = require('request');
 
 const app = express();
 const port = 3000;
+const apiDomain = process.env.API_DNS;
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+})
 
 app.get('/', (req, res) => {
   res.send('hello ');
+});
+
+app.get('/todo', (req, res) => {
+  let options = {
+    method: req.method,
+    uri: `${apiDomain}${req.url}`
+  };
+
+  request(options, (err, response) => {
+    res.send(response.body);
+  })
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
