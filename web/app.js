71007a146cd5efa6f8ce4a7c374c4637dac51c2d
Vivek Kumar Pandey <vivekp@thoughtworks.com>
2019-02-13 13:01:18 +0530

    integrated saveTodo post api to web

--- web/app.js
+++ web/app.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const request = require('request');
+const bodyParser = require('body-parser');
 
 const app = express();
+
 const port = 3000;
 const apiDomain = process.env.API_DNS;
 
+app.use(bodyParser.json());
 app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
 })
 
 app.get('/', (req, res) => {
-  res.send('hello ');
+  res.send('hello');
 });
 
-app.get('/todo', (req, res) => {
+app.all('/todo', (req, res) => {
   let options = {
     method: req.method,
+    body: req.body,
+    json: true,
     uri: `${apiDomain}${req.url}`
   };
 
